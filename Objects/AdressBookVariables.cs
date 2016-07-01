40b59d38099c71af1bf05de1d6c4986dd14264b3
Ben Logue <benjamin.logue73@gmail.com>
2016-07-01 08:08:37 -0700

    add contact variables, constructor, getters and ClearAll method.

--- Objects/AdressBookVariables.cs
+++ Objects/AdressBookVariables.cs
@@ -4,6 +4,37 @@ namespace AddressBook.Objects
 {
   private class Contact
   {
+    //Contact object variables
+    private string _name;
+    private string _address;
+    private string _phone;
+    private static List<Contact> _instances = new List<Contact> {};
 
+    //Contact object constructor
+    public Contact(string name, string address, string phone)
+    {
+      _name = name;
+      _address = address;
+      _phone = phone;
+      _instances.Add(this);
+    }
+
+    // Getters for object
+    public string GetName()
+    {
+      return _name;
+    }
+    public string GetAddress()
+    {
+      return _address
+    }
+    public string GetPhone()
+    {
+      return _phone;
+    }
+    public static List<Contact> GetAll()
+    {
+      return _instanaces;
+    }
   }
 }
