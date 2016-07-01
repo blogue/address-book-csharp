3d73994032906c05146cacf61e93fe6472952edd
Ben Logue <benjamin.logue73@gmail.com>
2016-07-01 08:34:11 -0700

    add routes in HomeModules.

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -8,7 +8,22 @@ namespace AddressBook
   {
     public HomeModule()
     {
-      
-    }
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+      Get["/add_contact"] = _ => {
+        return View["add_new_contact.cshtml"];
+      };
+      Get["/contacts"] = _ => {
+        List<Contact> allContacts = Contact.GetAll();
+        return View["list_all_contacts.cshtml", allJobs];
+      };
+      Get["/contacts/new"] = _ => {
+        return View["show_added_contact.cshtml"];
+      };
+      Post["/contacts/new"] = _ =>
+      Contact newContact = new Contact(Request.Form["name"], Request.Form["address"] Request.Form["phone"]);
+      return View["/show_added_contact.cshtml", newContact];
+    };
   }
 }
